612298fcf36dfa39f369c3baf637380e17fc7ce6
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-03-16 21:52:49 +0000

    initial commit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import PropTypes from 'prop-types';
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,11 +9,43 @@ import "bootstrap"
 // index.css'
 import '../styles/index.css'
 
-// components
-import Home from './components/Home';
+function SimpleCounter(props){
+  return (
+  <div className="container">
+    <div>🕔</div> 
+     <div>{props.digitoCuatro}</div>
+     <div>{props.digitoTres}</div>
+     <div>{props.digitoDos}</div>
+     <div>{props.digitoUno}</div>
+  </div>
+  
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+SimpleCounter.propTypes={
+  digitoCuatro:PropTypes.number,
+  digitoTres:PropTypes.number,
+  digitoDos:PropTypes.number,
+  digitoUno:PropTypes.number,
+
+}
+
+let counter = 0;
+
+setInterval(function(){
+  const cuatro = Math.floor(counter/1000);
+  const tres = Math.floor(counter/100);
+  const dos = Math.floor(counter/10);
+  const uno = Math.floor(counter/1);
+
+  counter++
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <SimpleCounter digitoCuatro={cuatro} digitoTres={tres} digitoDos={dos} digitoUno={uno}/>    
+    </React.StrictMode>,)
+
+}, 1000)
+
+
+

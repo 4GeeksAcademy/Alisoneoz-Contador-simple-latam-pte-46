215383f98212ccab668fa5ba67baee930e95a586
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-03-23 21:49:02 +0000

    add style

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,40 +9,43 @@ import "bootstrap"
 // index.css'
 import '../styles/index.css'
 
-function SimpleCounter(props){
+function SimpleCounter(props) {
   return (
-  <div className="container">
-    <div>🕔</div> 
-     <div>{props.digitoCuatro}</div>
-     <div>{props.digitoTres}</div>
-     <div>{props.digitoDos}</div>
-     <div>{props.digitoUno}</div>
-  </div>
-  
+    <div className="container">
+      <div className='clock'>🕔</div>
+      <div className="counter">
+        <div className='digit'>{props.digitoCuatro}</div>
+        <div className='digit inner'>{props.digitoTres}</div>
+        <div className='digit inner'>{props.digitoDos}</div>
+        <div className='digit'>{props.digitoUno}</div>
+      </div>
+
+    </div>
+
   )
 }
 
-SimpleCounter.propTypes={
-  digitoCuatro:PropTypes.number,
-  digitoTres:PropTypes.number,
-  digitoDos:PropTypes.number,
-  digitoUno:PropTypes.number,
+SimpleCounter.propTypes = {
+  digitoCuatro: PropTypes.number,
+  digitoTres: PropTypes.number,
+  digitoDos: PropTypes.number,
+  digitoUno: PropTypes.number,
 
 }
 
 let counter = 0;
 
-setInterval(function(){
-  const cuatro = Math.floor(counter/1000);
-  const tres = Math.floor(counter/100);
-  const dos = Math.floor(counter/10);
-  const uno = Math.floor(counter/1);
+setInterval(function () {
+  const cuatro = Math.floor(counter / 1000);
+  const tres = Math.floor(counter / 100);
+  const dos = Math.floor(counter / 10);
+  const uno = Math.floor(counter / 1);
 
   counter++
 
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <SimpleCounter digitoCuatro={cuatro} digitoTres={tres} digitoDos={dos} digitoUno={uno}/>    
+      <SimpleCounter digitoCuatro={cuatro} digitoTres={tres} digitoDos={dos} digitoUno={uno} />
     </React.StrictMode>,)
 
 }, 1000)
